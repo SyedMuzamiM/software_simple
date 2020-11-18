9176e9a179ad0201f21e76434aab68fd833a3303
Syed Muzamil <smmhd121@gmail.com>
2020-11-18 14:52:52 +0530

    Initial Commit

---
+++ data/grower.go
@@ -0,0 +1,28 @@
+package data
+
+import (
+)
+
+type Grower struct {
+	ID string
+	Name string
+	Adress string
+	Phone int64
+	Deliveries 
+}
+
+type Deliveries struct {
+	ID int
+	DriverName string
+	Mobile int
+	VechileNo string
+	ItemQty int
+	ItemName string
+	Date string
+}
+
+
+
+func main() {
+	return
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/syedmuzamilm/software_simple/data"
+)
+
+func main() {
+	fmt.Println("Welcome to the app")
+}
